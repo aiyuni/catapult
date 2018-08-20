0b5f5888ac95691ba4467af7163a17bab2b04a5b
Lena Kwan <lenakwan@gmail.com>
2018-08-20 16:16:12 -0700

    transitions work but the label isn't displaying/trying to make the label makes the menu state break

--- core/src/com/umm/randomgame/states/MenuState.java
+++ core/src/com/umm/randomgame/states/MenuState.java
@@ -1,16 +1,21 @@
 package com.umm.randomgame.states;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Event;
 import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.umm.randomgame.Main;
 
@@ -28,6 +33,11 @@ public class MenuState extends State {
     private TextureRegionDrawable drawableRegion;
     private Stage stage;
     private Texture realPlayButton;
+    private Label textLabel;
+    private Label.LabelStyle labelStyle;
+
+private BitmapFont font;
+private String text;
 
     public MenuState(GameStateManager gsm){
         super(gsm);
@@ -40,8 +50,15 @@ public class MenuState extends State {
         pressPlay = new ImageButton(drawableRegion);
         stage = new Stage(new ScreenViewport());
         stage.addActor(pressPlay);
-        pressPlay.setPosition((Main.WIDTH/2) - (playButton.getWidth()/2), (Main.HEIGHT/2));
+        pressPlay.setPosition(((Main.WIDTH/2) - (realPlayButton.getWidth()/2)), (Main.HEIGHT/2));
+        labelStyle = new Label.LabelStyle();
+        labelStyle.fontColor = Color.WHITE;
+        textLabel = new Label("Catapault", labelStyle);
+        stage.addActor(textLabel);
+        textLabel.setPosition(((Main.WIDTH/2)- (textLabel.getWidth()/2)), (Main.HEIGHT/3*2));
         Gdx.input.setInputProcessor(stage);
+
+
 
     }
     /*
@@ -53,6 +70,7 @@ public class MenuState extends State {
         pressPlay.addListener(new ClickListener(){
         @Override
         public boolean handle (Event event){
+
             gsm.set(new PlayState(gsm, main));
             dispose();
             return false;
@@ -77,12 +95,14 @@ public class MenuState extends State {
     stage.act(Gdx.graphics.getDeltaTime());
         sb.begin();
         sb.draw(background, 0,0, Main.WIDTH, Main.HEIGHT);
-        sb.draw(gameTitle, ((Main.WIDTH/2)- (gameTitle.getWidth()/2)), (Main.HEIGHT/3*2));
-       // sb.draw(playButton, (Main.WIDTH/2) - (playButton.getWidth()/2), (Main.HEIGHT/2));
-
-
-        stage.draw();
+//        font.draw(sb, text, Main.WIDTH/2,(Main.HEIGHT/2*3));
+// sb.draw(playButton, ((Main.WIDTH/2)- (gameTitle.getWidth()/2)), (Main.HEIGHT/3*2));
+ //sb.draw(playButton, (Main.WIDTH/2) - (playButton.getWidth()/2), (Main.HEIGHT/2));
         sb.end();
+
+
+       stage.draw();
+
 
     }
 
